8b41ba6fa3b7fa81921aa480d2f086b147b83438
Liem Ly <liemly98@gmail.com>
2022-06-27 15:08:34 +0700

    - init with daisy ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,12 @@ const Home: NextPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 };
